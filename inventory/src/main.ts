f6110d99cd51ec57d91c5dccd3e572bb46da1beb
ScrUbieNoobie <jivancharis143@gmail.com>
2023-11-20 22:14:13 +0800

    added env

--- inventory/src/main.ts
+++ inventory/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const appConfig = await NestFactory.create(AppModule);
+  const configService = appConfig.get(ConfigService);
+  const app = appConfig.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      port: configService.get<number>('INVENTORY_PORT'),
+    },
+  });
+  await app.listen();
 }
 bootstrap();
