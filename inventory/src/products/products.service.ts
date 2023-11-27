119caa3c46d919d298ec44c45d0412224df41cdb
ScrUbieNoobie <jivancharis143@gmail.com>
2023-11-27 23:45:38 +0800

    removed Logger

--- inventory/src/products/products.service.ts
+++ inventory/src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './products.entity';
 import { DeleteResult, Repository } from 'typeorm';
